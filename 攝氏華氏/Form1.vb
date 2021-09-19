ff716dbb9f0b5f31d699c6cf7988297b3bb8a113
RainWolf0603 <yohofan0603@gmail.com>
2021-09-19 14:27:50 +0800

    加入專案檔案。

---
+++ 攝氏華氏/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim F As Single
+        F = c2f(Val(TextBox1.Text))
+        TextBox2.Text = Math.Round(F, 2)
+    End Sub
+
+    Function c2f(ByVal n) As Single
+        c2f = n * (9 / 5) + 32
+    End Function
+End Class
